9e96ab28267f0068b32afafd271ddcc99ae6fdba
alvarorvn <48997399+alvarorvn@users.noreply.github.com>
2019-03-27 11:59:35 -0500

    Delete csvJson.js

--- csvJson.js
+++
@@ -1,9 +0,0 @@
-const csvToJson = require('csvtojson')
-
-const fileCSV = './customer-data.csv'
-
-csvToJson()
-    .fromFile(fileCSV)
-    .then((jsonObj) => {
-        console.log(jsonObj)
-    })
